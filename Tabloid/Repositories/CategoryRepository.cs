c201722ff0c09d6348ab08e5dab4ba1eda3ebca3
TarynL <tarlytle@gmail.com>
2021-07-14 16:55:21 -0500

    backend CRUD | manager fetch calls frontend

--- Tabloid/Repositories/CategoryRepository.cs
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -40,6 +40,93 @@ namespace Tabloid.Repositories
                 }
             }
         }
+
+        public Category GetById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                          SELECT Id, Name
+                          FROM Category
+                          
+                          WHERE Id = @Id
+                          ";
+
+                    DbUtils.AddParameter(cmd, "@Id", id);
+
+                    var reader = cmd.ExecuteReader();
+
+                    Category category = null;
+                    if (reader.Read())
+                    {
+                        category = new Category()
+                        {
+                            Id = id,
+                            Name = DbUtils.GetString(reader, "Name"),
+                        };
+                    }
+
+
+                    reader.Close();
+
+                    return category;
+                }
+            }
+        }
+
+        public void AddCategory (Category category)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Category (Name)
+                        OUTPUT INSERTED.ID
+                        VALUES ( @name)";
+                    DbUtils.AddParameter(cmd, "@name", category.Name);
+
+                    category.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+        public void Update(Category category)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        UPDATE Category
+                           SET name = @name,
+                         WHERE Id = @Id";
+
+                    DbUtils.AddParameter(cmd, "@name", category.Name);
+                    DbUtils.AddParameter(cmd, "@Id", category.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Category WHERE Id = @Id";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
    
--- Tabloid/client/src/modules/categoryManager.js
+++ Tabloid/client/src/modules/categoryManager.js
@@ -19,3 +19,87 @@ export const getAllCategories = () => {
         });
     });
 };
+
+export const addCategory = (category) => {
+    return getToken().then((token) => {
+
+        return fetch(baseUrl, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(category)
+        }).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else if (resp.status === 401) {
+                throw new Error("Unauthorized");
+            } else {
+                throw new Error("An unknown error occurred while trying to save a new category.");
+            }
+        });
+    });
+};
+
+
+export const getCategory = (id) => {
+    return getToken().then((token) => {
+
+        return fetch(`${baseUrl}/${id}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else {
+                throw new Error("An unknown error occurred while trying to get categories.");
+            }
+        });
+    });
+};
+
+export const updateCategory = (category) => {
+    return getToken().then((token) => {
+
+        return fetch(`${baseUrl}/${category.Id}`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(category)
+        }).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else if (resp.status === 401) {
+                throw new Error("Unauthorized");
+            } else {
+                throw new Error("An unknown error occurred while trying to save a new category.");
+            }
+        });
+    });
+};
+
+
+export const deleteCategory = (id) => {
+    return getToken().then((token) => {
+
+        return fetch(`${baseUrl}/${id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else {
+                throw new Error("An unknown error occurred while trying to delete category.");
+            }
+        });
+    });
+};
+
+
